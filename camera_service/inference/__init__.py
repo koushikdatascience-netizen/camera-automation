f31146c783e915f085c7b17892bca58cc7dc1063
Koushik Biswas <k.kbiswas8@gmail.com>
2026-09-26 00:08:36 +0530

    Export runtime backend selector

--- camera_service/inference/__init__.py
+++ camera_service/inference/__init__.py
@@ -6,10 +6,12 @@ from .ultralytics_adapter import UltralyticsCPUBackend, normalize_ultralytics_re
 from .gpu import UltralyticsGPUBackend, detect_local_gpu
 from .cloud import CloudGPUBackend
 from .factory import build_local_backends, choose_local_backend
+from .runtime import build_runtime_router, select_runtime_backend
 
 __all__ = [
     "InferenceBackend", "InferenceBackendRouter",
     "UltralyticsCPUBackend", "UltralyticsGPUBackend", "CloudGPUBackend",
     "normalize_ultralytics_results", "detect_local_gpu",
     "build_local_backends", "choose_local_backend",
+    "build_runtime_router", "select_runtime_backend",
 ]
